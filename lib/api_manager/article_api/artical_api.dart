a7f5227701b6e46fbdbe79a5da29df1d1d8a8072
Sefen farag <faragsefen@gmail.com>
2021-12-07 16:48:06 +0200

    update screen details and artical api

--- lib/api_manager/article_api/artical_api.dart
+++ lib/api_manager/article_api/artical_api.dart
@@ -2,22 +2,22 @@ import 'package:dio/dio.dart';
 import 'package:newyork_times/models/newyork/new_york.dart';
 
 abstract class BaseArticleApi {
-  Future<List<ArticleModel>> getArticleDateApi();
+  getArticleDateApi();
 }
 
 class ArticleApi extends BaseArticleApi {
-  late final dynamic _response;
   final String _baseUrl =
       "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=gFRTIQwAwAAgVvVemuXAprEFLTSs5hEG";
-  List<ArticleModel>? newyorkList = [];
+  List<ArticleModel> newyorkList = [];
 
   @override
   Future<List<ArticleModel>> getArticleDateApi() async {
-    _response = await Dio().get(_baseUrl);
-    final result = _response.data['results'] as List<dynamic>;
-    for (var element in result) {
-      newyorkList!.add(ArticleModel.fromMap(element as Map<String, dynamic>));
+    final _response = await Dio().get(_baseUrl);
+    final _result = _response.data['results'];
+
+    for (var element in _result) {
+      newyorkList.add(ArticleModel.fromMap(element as Map<String, dynamic>));
     }
-    return newyorkList!;
+    return newyorkList;
   }
 }
